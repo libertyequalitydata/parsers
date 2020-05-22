05986b3e5fd3fee82c9548a465d1e696b75df0a8
Ian Flom <ianrflom@gmail.com>
2020-05-22 11:01:44 -0500

    generalized parser algorithm, removed google parser alg

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import { basePaths } from "./config";
 
 const data = {
 	facebook: generalParser(basePaths.facebook),
+	spotify: generalParser(basePaths.spotify),
+	google: generalParser(basePaths.google),
 }
 
 const jsonResult = JSON.stringify(data,null,4);
--- parsers/generalParser.js
+++ parsers/generalParser.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { jsonToObject } from "../util/file-conversion";
+import { jsonToObject, csvToObject, vcfToObj } from "../util/file-conversion";
 
 export default getData;
 
@@ -8,29 +8,39 @@ function getData(basePath) {
 	return explore(basePath);
 
 	// explore: iterate through directory contents
-	// if json file: add to resultData
 	// if folder: explore()
-	function explore(directory){
+	function explore(directory) {
 		const dirList = fs.readdirSync(directory);
 
 		let resultData = new Object();
 
 		dirList.forEach(item => {
-			if (path.extname(item) == ".json"){
+			if (fs.lstatSync(`${directory}/${item}`).isDirectory()) {
+				resultData[item] = explore(`${directory}/${item}`);
+			}
+			else if (path.extname(item) == ".json") {
 				const rawData = jsonToObject(`${directory}/${item}`);
 				const keys = Object.keys(rawData);
-				const itemName = path.basename(item,".json");
+				const itemName = path.basename(item, ".json");
 
 				// If there is a wrapper key, use it
-				if (keys.length == 1){
+				if (keys.length == 1) {
 					resultData[keys[0]] = rawData[keys[0]];
 				} else {
 					resultData[itemName] = rawData;
 				}
 			}
-			else if (fs.lstatSync(`${directory}/${item}`).isDirectory()){
-				resultData[item] = explore(`${directory}/${item}`);
+			else if (path.extname(item) == ".csv"){
+				const rawData = csvToObject(`${directory}/${item}`);
+				const itemName = path.basename(item,".csv");
+				resultData[itemName] = rawData;
 			}
+			else if (path.extname(item) == ".vcf"){
+				const rawData = vcfToObj(`${directory}/${item}`);
+				const itemName = path.basename(item,".csv");
+				resultData[itemName] = rawData;
+			}
+
 		});
 		return resultData;
 	}
