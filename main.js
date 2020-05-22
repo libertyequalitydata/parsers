259a99e3ed9ec3bfd20617f725154fec44621931
Ian Flom <ianrflom@gmail.com>
2020-05-22 09:48:49 -0500

    rename facebook to general parser.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import facebook from "./parsers/facebook";
+import generalParser from "./parsers/generalParser";
 import fs from "fs";
 import { basePaths } from "./config";
 
 const data = {
-	facebook: facebook(basePaths.facebook),
+	facebook: generalParser(basePaths.facebook),
 }
 
 const jsonResult = JSON.stringify(data,null,4);
